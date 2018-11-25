86b10e4340cf5cc986be98deca671763a1d29679
irene <heirene@foxmail.com>
2018-11-25 14:50:41 +0800

    mark my serverinfo

---
+++ ServerInfo.py
@@ -0,0 +1,23 @@
+MyServerDict = {
+'Location': 'Tokyo',
+'IPV6address': '2001:19f0:7001:5ac7:5400:01ff:fec6:0e44',
+  'Ipv4Address' : '100.68.47.193',
+'uName':'root',
+'passaord':'$E8y$%BBBC.#o#t_',
+}
+'''直接按Ctrl + c
+
+附上一些其他较长使用的快捷键：
+
+ctrl + w —往回删除一个单词，光标放在最末尾 
+ctrl + k —往前删除到末尾，光标放在最前面（可以使用ctrl+a） 
+ctrl + u 删除光标以前的字符 
+ctrl + k 删除光标以后的字符 
+ctrl + a 移动光标至的字符头 
+ctrl + e 移动光标至的字符尾 
+ctrl + l 清屏
+--------------------- 
+作者：DreamLee0625 
+来源：CSDN 
+原文：https://blog.csdn.net/vitaminc4/article/details/76566720 
+版权声明：本文为博主原创文章，转载请附上博文链接！'''
